9692a4b8beae8bdad89379a08e72cc8423500fc6
1099368067@qq.com <qaz141995927->
2022-04-23 02:22:35 +0800

    update

---
+++ src/LRUCache.java
@@ -0,0 +1,41 @@
+import java.util.*;
+class LRUCache {
+    private int size;
+    private Map<Integer, Integer> map = new LinkedHashMap();
+    public LRUCache(int capacity) {
+        this.size = capacity;
+    }
+
+    public int get(int key) {
+        if (map.containsKey(key)){
+            int temp = map.get(key);
+            map.remove(key);
+            map.put(key, temp);
+            return temp;
+        }
+        return -1;
+
+    }
+
+    public void put(int key, int value) {
+        if (map.containsKey(key)){
+            map.remove(key);
+        }
+        else if (map.size() == size){
+            Iterator maplist = map.entrySet().iterator();
+            maplist.next();
+            maplist.remove();
+        }
+
+        map.put(key, value);
+
+    }
+}
+
+
+/**
+ * Your LRUCache object will be instantiated and called as such:
+ * LRUCache obj = new LRUCache(capacity);
+ * int param_1 = obj.get(key);
+ * obj.put(key,value);
+ */
--- src/iter.java
+++ src/iter.java
@@ -9,12 +9,25 @@ public class iter {
             list.add(new String("list" + i));
             map.put(i, new String("map" + i));
         }
+        System.out.println(map.size());
 
         Iterator iterlist = list.iterator();
         Iterator maplist = map.entrySet().iterator();
         while(maplist.hasNext()){
             System.out.println(maplist.next());
         }
-
+        System.out.println(map.containsKey(11));
+        map.put(10, null);
+        System.out.println(map.isEmpty());
+        System.out.println("ありがとう");
+        map.remove(0);
+        map.put(0, "map10");
+        map.remove(5);
+        map.put(5, "map11");
+        Iterator maplist1 = map.entrySet().iterator();
+        System.out.println(map);
+        while(maplist1.hasNext()){
+            System.out.println(maplist1.next());
+        }
     }
 }
