0d0f98ce25c45dc0f8ce592cf06957281adf6e42
ippousyuga <1099368067@qq.com>
2022-05-08 23:27:22 +0800

    may 8th

---
+++ src/May/Sum_IV.java
@@ -0,0 +1,33 @@
+package May;
+
+import java.util.*;
+
+public class Sum_IV {
+    public List<List<Integer>> fourSum(int[] nums, int target) {
+        Arrays.sort(nums);
+        List<List<Integer>> result = new ArrayList<>();
+        for (int i = 0; i < nums.length; i++){
+            if (nums[i] > 0) break;
+            if (i > 0 && nums[i] == nums[i - 1]) continue;;
+            for (int j = i + 1; j < nums.length; j++){
+                if (j > i + 1 && nums[j] == nums[j - 1]) continue;
+                int left = j + 1;
+                int right = nums.length - 1;
+                while (left < right){
+                    int sum = nums[i] + nums[j] + nums[left] + nums[right];
+                    if (sum < target) left++;
+                    else if (sum > target) right--;
+                    else {
+                        result.add(Arrays.asList(nums[i], nums[j], nums[left], nums[right]));
+                        while (left < right && nums[left + 1] == nums[left]) left++;
+                        while (left < right && nums[right - 1] == nums[right]) right--;
+                        left++;
+                        right--;
+                    }
+
+                }
+            }
+        }
+        return result;
+    }
+}
