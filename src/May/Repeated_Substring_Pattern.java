b60162dd85fda3eefe3705f09ad615456a9ce72f
1099368067@qq.com <qaz141995927->
2022-05-16 02:14:21 +0800

    May 16th

--- src/May/Repeated_Substring_Pattern.java
+++ src/May/Repeated_Substring_Pattern.java
@@ -1,10 +1,23 @@
 package May;
+/*
+* 459. 重复的子字符串
+* KMP应用
+* next[len - 1]就是该字符串最长相同前后缀长度，len - next[len - 1]为最短重复字符串长度
+* 取余为0即可通过某字符串重复构成s。
+* KMP的next：
+* 初始化j = 0; next[0] = 0;
+* for(i = 1)
+*   while(j > 0 && 前后缀不同（charAt(i) != charAt(j)）) j = next[j - 1]（回退）
+*   if(前后缀相同 （charAt(i) == charAt(j)）) j++
+* next[i] = j;
+* */
 
 public class Repeated_Substring_Pattern {
     public boolean repeatedSubstringPattern(String s) {
+        if (s == "") return false;
         int [] next = next(s);
         int len = s.length();
-        if (next[len] > 0 && len % (len - next[len]) == 0) {
+        if (next[len - 1] != 0 && len % (len - next[len - 1]) == 0) {
             return true;
         }
         return false;
--- src/May/test.java
+++ src/May/test.java
@@ -1,11 +1,10 @@
 package May;
 
 import java.util.Arrays;
-import java.util.List;
 
 public class test {
     public static void main(String[] args){
-        String a = "abba";
+        String a = "abaab";
         String b = "issipi";
         int target = 20;
         Repeated_Substring_Pattern repeated_substring_pattern = new Repeated_Substring_Pattern();
