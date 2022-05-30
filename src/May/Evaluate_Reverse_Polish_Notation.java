bb3ba8b67e5a54dae4bda92d2be7f7bea8003ade
1099368067@qq.com <qaz141995927->
2022-05-31 02:35:15 +0800

    May 31th

--- src/May/Evaluate_Reverse_Polish_Notation.java
+++ src/May/Evaluate_Reverse_Polish_Notation.java
@@ -1,7 +1,41 @@
 package May;
+import java.util.*;
+/*
+* 注意int string互相转换用valueOf
+* 记得判断是否为两位数及以上（code 13
+* */
 
 public class Evaluate_Reverse_Polish_Notation {
     public int evalRPN(String[] tokens) {
-
+        Stack<String> stack = new Stack<>();
+        int nums3 = 0;
+        for (String token : tokens){
+            if (token.length() <= 1 && (token.charAt(0) > '9' || token.charAt(0) < '0')){
+                int nums1 = Integer.valueOf(stack.pop());
+                int nums2 = Integer.valueOf(stack.pop());
+                if (token.charAt(0) == '+'){
+                    nums3 = nums1 + nums2;
+                    stack.push(String.valueOf(nums3));
+                    continue;
+                }
+                if (token.charAt(0) == '-'){
+                    nums3 = nums2 - nums1;
+                    stack.push(String.valueOf(nums3));
+                    continue;
+                }
+                if (token.charAt(0) == '*'){
+                    nums3 = nums1 * nums2;
+                    stack.push(String.valueOf(nums3));
+                    continue;
+                }
+                if (token.charAt(0) == '/'){
+                    nums3 = nums2 / nums1;
+                    stack.push(String.valueOf(nums3));
+                    continue;
+                }
+            }
+            else  stack.push(token);
+        }
+        return Integer.valueOf(stack.pop());
     }
 }
--- src/May/test.java
+++ src/May/test.java
@@ -2,12 +2,12 @@ package May;
 
 public class test {
     public static void main(String[] args){
-        String a = "abbaca";
+        String a[] = {"10","6","9","3","+","-11","*","/","*","17","+","5","+"};
         String b = "issipi";
         int [] nums = {2,3};
         int target = 2;
-        Remove_All_Adjacent_Duplicates_In_String remove_all_adjacent_duplicates_in_string = new Remove_All_Adjacent_Duplicates_In_String();
-        String result = remove_all_adjacent_duplicates_in_string.removeDuplicates(a);
+        Evaluate_Reverse_Polish_Notation evaluate_reverse_polish_notation = new Evaluate_Reverse_Polish_Notation();
+        int result = evaluate_reverse_polish_notation.evalRPN(a);
 
         System.out.println(result);
 
