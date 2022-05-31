cee17a0cc1c55161bf2604f3a9770b90a40422b9
1099368067@qq.com <qaz141995927->
2022-06-01 01:49:46 +0800

    June 1st

--- src/May/Sliding_Window_Maximum.java
+++ src/May/Sliding_Window_Maximum.java
@@ -2,7 +2,10 @@ package May;
 import java.util.*;
 /*
 * 用双向队列存储滑块内最大的数组索引。
-* 当队列不为空 &&
+* 当队列不为空 && 队列顶元素 < 滑块内的索引 ——》 出队
+* while(队列不为空 && 队尾元素对应数组值 < 当前循环所指数组值) ——》 队尾出队
+* 每次循环后 i 入队。
+* 当i >= k - 1 每次循环把队首入result数组
 * */
 
 
@@ -13,7 +16,7 @@ public class Sliding_Window_Maximum {
         int []res = new int[n - k + 1];
         int idx = 0;
         for (int i = 0; i < n; i++){
-            while (! arrayDeque.isEmpty() && arrayDeque.peek() < i - k + 1){
+            if (! arrayDeque.isEmpty() && arrayDeque.peek() < i - k + 1){
                 arrayDeque.poll();
             }
             while (! arrayDeque.isEmpty() && nums[arrayDeque.peekLast()] < nums[i]){
--- src/May/test.java
+++ src/May/test.java
@@ -4,10 +4,10 @@ public class test {
     public static void main(String[] args){
         String a[] = {"10","6","9","3","+","-11","*","/","*","17","+","5","+"};
         String b = "issipi";
-        int [] nums = {2,3};
-        int target = 2;
-        Evaluate_Reverse_Polish_Notation evaluate_reverse_polish_notation = new Evaluate_Reverse_Polish_Notation();
-        int result = evaluate_reverse_polish_notation.evalRPN(a);
+        int [] nums = {1,3,-1,-3,5,3,6,7};
+        int target = 3;
+        Sliding_Window_Maximum sliding_window_maximum = new Sliding_Window_Maximum();
+        int []result = sliding_window_maximum.maxSlidingWindow(nums, target);
 
         System.out.println(result);
 
