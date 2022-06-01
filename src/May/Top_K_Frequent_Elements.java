63d4d01b36fad4533344194350c0b2f993ee21e1
ippousyuga <1099368067@qq.com>
2022-06-02 02:21:24 +0800

    June 2nd

--- src/May/Top_K_Frequent_Elements.java
+++ src/May/Top_K_Frequent_Elements.java
@@ -9,6 +9,19 @@ public class Top_K_Frequent_Elements {
             if (hashMap.containsKey(num)) hashMap.replace(num, hashMap.get(num) + 1);
             else hashMap.put(num, 0);
         }
-        return nums;
+        PriorityQueue<Integer> pq = new PriorityQueue<>((a, b) -> hashMap.get(a) - hashMap.get(b));
+
+        for (int key : hashMap.keySet()){
+            if (pq.size() < k) pq.offer(key);
+            else if (hashMap.get(key) > hashMap.get(pq.peek())){
+                pq.poll();
+                pq.offer(key);
+            }
+        }
+        int res [] = new int[k];
+        for (int i = 0; i < k; i++){
+            res[i] = pq.poll();
+        }
+        return res;
     }
 }
