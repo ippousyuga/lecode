ec1fd388e5953425b26fb899496b61372dcc993f
ippousyuga <1099368067@qq.com>
2022-06-19 13:34:47 +0800

    June 19th

---
+++ src/June/TreeNode.java
@@ -0,0 +1,18 @@
+package June;
+
+public class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    public TreeNode getLeft() {
+        return left;
+    }
+    public TreeNode getRight() {
+        return right;
+    }
+
+    public int getVal() {
+        return val;
+    }
+}
