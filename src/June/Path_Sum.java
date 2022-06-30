e267060b5de4aa950dc48bdb559212c62d985304
1099368067@qq.com <qaz141995927->
2022-06-30 14:12:51 +0800

    June 30th

---
+++ src/June/Path_Sum.java
@@ -0,0 +1,11 @@
+package June;
+
+public class Path_Sum {
+    public boolean hasPathSum(TreeNode root, int targetSum) {
+
+    }
+    public boolean PathCheck(TreeNode treeNode, int target){
+
+    }
+
+}
