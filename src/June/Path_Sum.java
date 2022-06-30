80f56e1f68192494b0d1e08c7ffd5d3f8aa3fdc0
ippousyuga <1099368067@qq.com>
2022-07-01 00:32:29 +0800

    July 1st

--- src/June/Path_Sum.java
+++ src/June/Path_Sum.java
@@ -1,11 +1,19 @@
 package June;
+/*
+*112. 路径总和
+* */
 
 public class Path_Sum {
     public boolean hasPathSum(TreeNode root, int targetSum) {
-
+        if (root == null) return false;
+        int sum = 0;
+        PathCheck(root, targetSum, sum);
     }
-    public boolean PathCheck(TreeNode treeNode, int target){
-
+    public boolean PathCheck(TreeNode treeNode, int target, int sum){
+        if (treeNode == null) return false;
+        sum += treeNode.val;
+        if (sum == target & treeNode.left == null & treeNode.right == null) return true;
+        return PathCheck(treeNode.left, target, sum) & PathCheck(treeNode.right, target, sum);
     }
 
 }
