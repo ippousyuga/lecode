62cf6b2892398cf86e38b487b465781d0083e382
1099368067@qq.com <qaz141995927->
2022-07-01 13:24:47 +0800

    July 1st

---
+++ src/June/Construct_Binary_Tree_from_Inorder_and_Postorder_Traversal.java
@@ -0,0 +1,7 @@
+package June;
+
+public class Construct_Binary_Tree_from_Inorder_and_Postorder_Traversal {
+    public TreeNode buildTree(int[] inorder, int[] postorder) {
+
+    }
+}
--- src/June/Path_Sum.java
+++ src/June/Path_Sum.java
@@ -7,13 +7,13 @@ public class Path_Sum {
     public boolean hasPathSum(TreeNode root, int targetSum) {
         if (root == null) return false;
         int sum = 0;
-        PathCheck(root, targetSum, sum);
+        return PathCheck(root, targetSum, sum);
     }
     public boolean PathCheck(TreeNode treeNode, int target, int sum){
         if (treeNode == null) return false;
         sum += treeNode.val;
         if (sum == target & treeNode.left == null & treeNode.right == null) return true;
-        return PathCheck(treeNode.left, target, sum) & PathCheck(treeNode.right, target, sum);
+        return PathCheck(treeNode.left, target, sum) || PathCheck(treeNode.right, target, sum);
     }
 
 }
