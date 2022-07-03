8116d389abaefb445b40471e8efe5bf2bff1a919
ippousyuga <1099368067@qq.com>
2022-07-04 01:01:41 +0800

    July 4th

--- src/April/test.java
+++ src/April/test.java
@@ -3,23 +3,23 @@ package April;
 import java.util.Arrays;
 
 public class test {
-    public static ListNode reverseList(ListNode head) {
-        ListNode cur = head;
-        ListNode pev = null;
-        while (cur != null){
-            ListNode temp = cur.next;
-            cur.next = pev;
-            pev = cur;
-            cur = temp;
-        }
-        System.out.println("ありがとう！");
-        return pev;
-    }
-    public static void main(String[] args){
-        ListNode head = new ListNode();
-        int[] test = {1};
-        Arrays.copyOfRange(test, 1, 1);
-
-        reverseList(null);
-    }
+//    public static ListNode reverseList(ListNode head) {
+//        ListNode cur = head;
+//        ListNode pev = null;
+//        while (cur != null){
+//            ListNode temp = cur.next;
+//            cur.next = pev;
+//            pev = cur;
+//            cur = temp;
+//        }
+//        System.out.println("ありがとう！");
+//        return pev;
+//    }
+//    public static void main(String[] args){
+//        ListNode head = new ListNode();
+//        int[] test = {1};
+//        Arrays.copyOfRange(test, 1, 1);
+//
+//        reverseList(null);
+//    }
 }
--- src/July/Maximum_Binary_Tree.java
+++ src/July/Maximum_Binary_Tree.java
@@ -1,5 +1,33 @@
 package July;
 
+import June.TreeNode;
+
+import java.util.Arrays;
+/*
+* 654. 最大二叉树
+* */
+
 public class Maximum_Binary_Tree {
+    public TreeNode constructMaximumBinaryTree(int[] nums) {
+        return construct(nums);
+    }
+    public TreeNode construct(int[] nums){
+        if (nums.length == 0) return null;
+        int max = nums[0];
+        int max_index = 0;
+        for (int i = 0; i < nums.length; i++){
+            if (nums[i] > max) {
+                max = nums[i];
+                max_index = i;
+            }
+        }
+        TreeNode treeNode = new TreeNode(max);
+        int[] left_child  = Arrays.copyOfRange(nums, 0, max_index);
+        int[] right_child = Arrays.copyOfRange(nums, max_index + 1, nums.length);
+        treeNode.left = construct(left_child);
+        treeNode.right = construct(right_child);
+        return treeNode;
+
+    }
 
 }
