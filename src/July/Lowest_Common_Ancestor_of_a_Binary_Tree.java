c22724d5ff80006865acba4d292d48d8fc576e11
1099368067@qq.com <qaz141995927->
2022-07-06 12:50:13 +0800

    July 6th

---
+++ src/July/Lowest_Common_Ancestor_of_a_Binary_Tree.java
@@ -0,0 +1,18 @@
+package July;
+
+import June.TreeNode;
+
+public class Lowest_Common_Ancestor_of_a_Binary_Tree {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if (root == null || root == p || root == q){
+            return root;
+        }
+        TreeNode left = lowestCommonAncestor(root.left, p, q);
+        TreeNode right = lowestCommonAncestor(root.right, p, q);
+
+        if (left == null & right != null) return right;
+        else if (left != null & right == null) return left;
+        return root;
+    }
+
+}
