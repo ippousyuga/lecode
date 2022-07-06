1c8ea4bdb57f3bf08f16c0773eab77f5ca70d0e0
ippousyuga <1099368067@qq.com>
2022-07-06 14:21:56 +0800

    July 5th

--- src/July/Lowest_Common_Ancestor_of_a_Binary_Tree.java
+++ src/July/Lowest_Common_Ancestor_of_a_Binary_Tree.java
@@ -1,6 +1,14 @@
 package July;
 
 import June.TreeNode;
+/*
+* 236. 二叉树的最近公共祖先
+* 递归过程
+* 如果找到p或q就return 该节点
+* 如果最后找到了p和q则return root即为祖先节点
+* 如果只找到p或q中的一个，则return left或right即找到p或q的存储节点
+* 否则return null
+* */
 
 public class Lowest_Common_Ancestor_of_a_Binary_Tree {
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
@@ -12,6 +20,7 @@ public class Lowest_Common_Ancestor_of_a_Binary_Tree {
 
         if (left == null & right != null) return right;
         else if (left != null & right == null) return left;
+        else if(left == null && right == null) return null;
         return root;
     }
 
