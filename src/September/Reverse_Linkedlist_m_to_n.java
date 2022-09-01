849470408d50f4c3bb503aad5aa0959c96d0cda8
ippousyuga <1099368067@qq.com>
2022-09-02 02:03:44 +0800

    September 1st

--- src/September/Reverse_Linkedlist_m_to_n.java
+++ src/September/Reverse_Linkedlist_m_to_n.java
@@ -2,8 +2,16 @@ package September;
 
 import May.ListNode;
 /*
+* 92. 反转链表 II
 * 美团一面面经题
 * 反转链表的m到n个节点
+* 首先需要找到第m-1个节点为per，第n个节点为cur，
+* 需要画图确定两种情况
+* 一种是m不为1的时候，需要找到per为第m-1个节点，然后先让temp=per,per.next = cur,然后让cur=cur.next,per=temp.next
+* 即先把m左边的边界的next确定好
+* 然后再按照快慢指针进行更换
+* 第二种如果m=1，则当作普通的反转链表（1到n）即让cur=cur.next
+* 然后第二种return的是最后一次交换的cur
 * */
 
 public class Reverse_Linkedlist_m_to_n {
@@ -16,17 +24,20 @@ public class Reverse_Linkedlist_m_to_n {
             }
             cur = cur.next;
         }
-
         ListNode temp = per.next;
-        per.next = cur;
-        cur = cur.next;
-        per = temp;
+        if (m > 1){
+            per.next = cur;
+            cur = cur.next;
+            per = temp;
+        }
+        else cur = cur.next;
         for (int i = 0; i <= n - m; i++){
             temp = per.next;
             per.next = cur;
             cur = per;
             per = temp;
         }
+        if (m == 1) return cur;
         return head;
     }
 
@@ -38,7 +49,7 @@ public class Reverse_Linkedlist_m_to_n {
         first.next.next.next.next = new ListNode(5);
         first.next.next.next.next.next = new ListNode(6);
         Reverse_Linkedlist_m_to_n reverse_linkedlist_m_to_n = new Reverse_Linkedlist_m_to_n();
-        ListNode listNode = reverse_linkedlist_m_to_n.reverseMtoN(first, 3, 5);
+        ListNode listNode = reverse_linkedlist_m_to_n.reverseMtoN(first, 1, 2);
         while (listNode != null){
             System.out.println(listNode.val);
             listNode = listNode.next;
